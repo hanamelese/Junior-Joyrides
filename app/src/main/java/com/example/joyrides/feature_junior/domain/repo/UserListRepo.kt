4bbf6f64589db8c74a123094c6cd94f60c341b73
rita <ritaayalew6@gmail.com>
2025-05-12 09:49:33 +0300

    make the user entity repository interface

---
+++ app/src/main/java/com/example/joyrides/feature_junior/domain/repo/UserListRepo.kt
@@ -0,0 +1,18 @@
+package com.example.joyrides.feature_junior.domain.repo
+
+import com.example.trial_junior.feature_junior.domain.model.UserItem
+
+interface UserListRepo {
+    suspend fun registerUser(firstName: String, lastName: String, email: String, password: String): UserItem
+    suspend fun loginUser(email: String, password: String): String
+    suspend fun getMyProfile(): UserItem
+    suspend fun updateProfile(
+        email: String,
+        firstName: String?,
+        lastName: String?,
+        newEmail: String?,
+        password: String?,
+        profileImageUrl: String?,
+        backgroundImageUrl: String?
+    ): UserItem
+}
