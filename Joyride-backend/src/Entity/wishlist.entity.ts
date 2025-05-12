8e178c5a93c06df446ad8a367f7f72e0f6674cc6
hanamelese <hanamelese95@gmail.com>
2025-05-12 08:02:24 -0700

    create an entity

---
+++ Joyride-backend/src/Entity/wishlist.entity.ts
@@ -0,0 +1,41 @@
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from "typeorm";
+import { UserEntity } from "./user.entity";
+
+@Entity()
+export class WishListEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    childName: string;
+
+    @Column()
+    guardianEmail: string;
+
+    @Column()
+    age: number;
+
+    @Column()
+    date: string;
+
+    @Column()
+    specialRequests: string;
+
+    @Column()
+    imageUrl: string;
+
+    @Column()
+    upcoming: boolean;
+
+    @Column()
+    userId: number;
+
+    @ManyToOne(() => UserEntity, user => user.wishLists)
+    @JoinColumn({ name: 'userId' })
+    user: UserEntity;
+}
+
+export enum WishListStatus {
+    upComing = 'UPCOMING',
+    posted = 'POSTED'
+}
