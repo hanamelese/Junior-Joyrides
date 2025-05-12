442d39b13962d84b6a26dd99fcc9b1d1235d53e1
rita <ritaayalew6@gmail.com>
2025-05-12 09:51:32 +0300

    make the necessary usecases for the user related with authentication and authorization

---
+++ app/src/main/java/com/example/joyrides/feature_junior/domain/use_case/UserUseCases.kt
@@ -0,0 +1,37 @@
+package com.example.joyrides.feature_junior.domain.use_case
+
+import com.example.trial_junior.feature_junior.domain.model.UserItem
+import com.example.trial_junior.feature_junior.domain.repo.UserListRepo
+import javax.inject.Inject
+
+data class UserUseCases @Inject constructor(
+    private val repository: UserListRepo
+) {
+    suspend fun registerUser(firstName: String, lastName: String, email: String, password: String): UserItem =
+        repository.registerUser(firstName, lastName, email, password)
+
+    suspend fun loginUser(email: String, password: String): String =
+        repository.loginUser(email, password)
+
+    suspend fun getMyProfile(): UserItem =
+        repository.getMyProfile()
+
+    suspend fun updateProfile(
+        email: String,
+        firstName: String?,
+        lastName: String?,
+        newEmail: String?,
+        password: String?,
+        profileImageUrl: String?,
+        backgroundImageUrl: String?
+    ): UserItem {
+        return repository.updateProfile(
+            email,
+            firstName,
+            lastName,
+            newEmail,
+            password,
+            profileImageUrl,
+            backgroundImageUrl
+        )
+    }}
